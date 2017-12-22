a06485427c853aa777e2b166e17a289bbad9bfe2
Sergei Pogorelov <pogorelov.ss@gmail.com>
2017-12-22 16:53:11 +0200

    accounts

--- apps/jwt_user/urls.py
+++ apps/jwt_user/urls.py
@@ -5,11 +5,13 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 
-router = DefaultRouter()
-router.register(r'users', views.ReadOnlyUserViewSet)
+# router = DefaultRouter()
+# router.register(r'users', views.ReadOnlyUserViewSet)
 
 urlpatterns = [
-    url(r'^users/', include(router.urls)),
-    url(r'^accounts/signup/$', views.UserSignUpView.as_view(), name='user-signup'),
-    url(r'^accounts/profile/$', views.MyUserView.as_view(), name='my-user-view'),
+    # url(r'^users/', include(router.urls)),
+    url(r'^login/$', views.UserLoginView.as_view(), name='user-login'),
+    url(r'^logout/$', views.UserLogoutView.as_view(), name='user-logout'),
+    url(r'^obtain-jwt/$', views.ObtainJSONWebToken.as_view(), name='obtain-jwt'),
+    # url(r'^profile/$', views.MyUserView.as_view(), name='my-user-view'),
 ]
--- apps/jwt_user/views.py
+++ apps/jwt_user/views.py
@@ -1,6 +1,7 @@
 from datetime import timedelta, datetime
 
 from django.contrib.auth import login, logout
+from rest_framework import status
 
 from rest_framework.response import Response
 from rest_framework.views import APIView
