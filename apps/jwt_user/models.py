fe84007f6d640039250d2fa79d5bc9d5477e1b53
Sergei Pogorelov <pogorelov.ss@gmail.com>
2018-01-05 12:02:09 +0200

    doctest support

--- apps/jwt_user/models.py
+++ apps/jwt_user/models.py
@@ -6,4 +6,3 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     uuid = models.UUIDField(unique=True, editable=False, default=uuid.uuid4, verbose_name='User public identifier')
-
